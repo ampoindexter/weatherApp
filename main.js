548cc065822ca2c08c373c2c9f6f62ac489bde3b
Alicia Poindexter <alcampbel@gmail.com>
2016-01-15 23:08:29 -0800

    clean up display

--- main.js
+++ main.js
@@ -5,25 +5,27 @@ $(document).ready(init);
 var apiUrl = 'http://api.wunderground.com/api/2a9c2964632db9eb/';
 
 function init() {
-  $.ajax("http://api.wunderground.com/api/2a9c2964632db9eb/geolookup/q/autoip.json", {
+  getLocation();
+  $('#getWeather').click(getWeather);
+}
+
+function getLocation() {
+  $.ajax(apiUrl + "geolookup/q/autoip.json", {
     success: function(data) {
       var defaultZipCode = data.location.zip;
       $('#location').val(defaultZipCode);
     }
   }) 
-   $('#getWeather').click(getWeather);
 }
 
-
-
-
-function getWeather (){
+function getWeather () {
   var location = $('#location').val();
   var url = apiUrl + 'conditions/q/' + location + '.json';
   
 
   $.get(url)
   .done(function(data){
+    console.log(data);
     var currentTemp = (data.current_observation.temperature_string) + ' ' ;
     var currentImage = data.current_observation.icon_url;
     var currentWind = data.current_observation.wind_string;
@@ -38,12 +40,8 @@ function getWeather (){
     $('#currentWind').append(currentWind);
     $('#currentTime').append('Your current local time is: ')
     $('#currentTime').append(currentTime);
-
   })
   .fail(function(error){
     console.error(error);
   });
-
-
-
 }
